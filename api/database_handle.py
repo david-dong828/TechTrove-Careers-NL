fdfe2f4daa17293d2dd040ef8df8046e2abd9dd4
david-dong828 <david.dong828@gmail.com>
2024-01-28 20:47:36 -0330

    update structure for vercel

--- api/database_handle.py
+++ api/database_handle.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 import mysql.connector
+import MySQLdb
 
 
 def connectDB():
@@ -16,16 +17,16 @@ def connectDB():
     # )
 
     # For Vercel, using PlanetScale
-    db = mysql.connector.connect(
+    db = MySQLdb.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USERNAME"),
         passwd=os.getenv("DB_PASSWORD"),
         db=os.getenv("DB_NAME"),
-        # autocommit=True,
-        # ssl_mode="VERIFY_IDENTITY",
-        # ssl={
-        #     "ca": "/etc/ssl/cert.pem"
-        # }
+        autocommit=True,
+        ssl_mode="VERIFY_IDENTITY",
+        ssl={
+            "ca": "/etc/ssl/cert.pem"
+        }
     )
 
     cursor = db.cursor()
